bb6bf48acec6c532d35163326205a5e437b85890
Rune Piper <rune@visuellverstehen.de>
2017-10-16 16:33:00 +0200

    [CLEANUP] Simplify extension structure

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,10 +5,36 @@ defined('TYPO3_MODE') || die();
     'VV.' . $_EXTKEY,
     'Pi1',
     [
-        'DataHarvest' => 'gatherData',
+        'DataHarvest' => 'gatherData'
     ],
     // non-cacheable action(s)
     [
-        'DataHarvest' => 'gatherData',
+        'DataHarvest' => 'gatherData'
     ]
 );
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
+    t3viewGatherDataHarvest = PAGE
+    t3viewGatherDataHarvest {
+        typeNum = 5996
+
+        config {
+            disableAllHeaderCode = 1
+            additionalHeaders {
+                10.header = Content-Type: application/javascript;charset=UTF-8
+            }
+            xhtml_cleaning = 0
+        }
+
+        1000 = USER_INT
+        1000 {
+            userFunc = TYPO3\CMS\Extbase\Core\Bootstrap->run
+
+            vendorName = VV
+            extensionName = T3view
+            pluginName = Pi1
+            controller = DataHarvest
+            action = gatherData
+        }
+    }
+');
