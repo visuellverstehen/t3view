47264b7de2e0470a8d8e8cbb558a47423cb7a7b5
Rune Piper <rune.piper@visuellverstehen.de>
2017-10-17 15:41:51 +0200

    [TASK] Add request authentication

--- Classes/Controller/DataHarvestController.php
+++ Classes/Controller/DataHarvestController.php
@@ -1,6 +1,10 @@
 <?php
 namespace VV\T3view\Controller;
 
+use TYPO3\CMS\Core\Error\Http\StatusException;
+use TYPO3\CMS\Core\Error\Http\UnauthorizedException;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\HttpUtility;
 use TYPO3\CMS\Extensionmanager\Utility\ConfigurationUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TYPO3\CMS\Extbase\Mvc\View\JsonView;
@@ -15,6 +19,14 @@ use VV\T3view\Service\SystemInformationService;
 class DataHarvestController extends ActionController
 {
     /**
+     * Holds all allowed HTTP methods. Other methods
+     * will throw an exception.
+     *
+     * @var array
+     */
+    const ALLOWED_HTTP_METHODS = ['GET'];
+
+    /**
      * @var JsonView
      */
     protected $defaultViewObjectName = JsonView::class;
@@ -28,13 +40,31 @@ class DataHarvestController extends ActionController
      * This method will be dispatche before the actual method. This allows
      * us to do some security checks before performing actual stuff.
      *
-     * - Checks if the HTTP method is GET. Other methods are not allowed.
+     * - Checks if the HTTP method is allowed. Other methods are not allowed.
      * - Checks if the requests secret is the same as the stored secret.
      */
     public function initializeGatherDataAction() {
-        //$configurationUtility = $this->objectManager->get(ConfigurationUtility::class);
-        //$extensionConfiguration = $configurationUtility->getCurrentConfiguration(strtolower($this->extensionName));
-        //$secret = $extensionConfiguration['secret']['value'];
+        if (!in_array($this->request->getMethod(), self::ALLOWED_HTTP_METHODS)) {
+            throw new StatusException(
+                HttpUtility::HTTP_STATUS_405,
+                'The method [' . $this->request->getMethod() . '] is not allowed by the controller [' . __CLASS__ . '].',
+                'Oops, an error occurred!',
+                1508246619
+            );
+        }
+
+        // Getting the secret through the TYPO3 API instead of accessing it directly.
+        $configurationUtility = $this->objectManager->get(ConfigurationUtility::class);
+        $extensionConfiguration = $configurationUtility->getCurrentConfiguration(strtolower($this->extensionName));
+        $secret = $extensionConfiguration['secret']['value'];
+
+        // Authenticate request
+        if ($secret !== GeneralUtility::_GET('secret')) {
+            throw new UnauthorizedException(
+                'Accessing this page requires authorization.',
+                1508246620
+            );
+        }
     }
 
     /**
